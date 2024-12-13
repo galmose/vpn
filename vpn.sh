4decfe2e00088d0fc3ac2f99adffe8b588afbd70
Tidiane Savadogo <166531945+galmose@users.noreply.github.com>
2024-12-13 19:00:19 +0000

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Installer Tailscale
-curl -fsSL https://tailscale.com/install.sh | sh
+# Installer Tailscale en ignorant la vérification des certificats
+curl -k -fsSL https://tailscale.com/install.sh | sh
 
 # Activer Tailscale avec une clé d'authentification
 sudo tailscale up --auth-key=tskey-auth-kYhuRBFFZ121CNTRL-ZiDv1mQaos3iAL3Sb5sAt3wKsjfi731We
